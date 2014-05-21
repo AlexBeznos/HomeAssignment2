091ef13b1706bce5fc82847c28655f8976701717
Alex Beznos <beznosa@yahoo.com>
2014-05-22 02:42:16 +0300

    Add robot variable to update action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,12 @@ class HomeController < ApplicationController
   def update
   	file = params[:input_data]
   	if File.extname(file) == '.txt'
+  		Robot.destroy_all
   		File.new(file, "r").each { |line| Robot.inputer(line)}
   	else
   		redirect_to root_path, alert: "You have uploaded wrong type of file! Try to reload it!"
   	end
+  	@robots = Robot.all
   end
 
   def research
